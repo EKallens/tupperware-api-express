43d2dd85f9842aa669749a1ece78725900284584
Edwin Kallens Padilla <e.kallensp@gmail.com>
2024-11-24 12:27:39 -0300

    feat: implement auth login and register in presentation layer

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,15 +1,38 @@
 import { Request, Response } from 'express'
+import { RegisterUserDto } from '@/domain/dtos/auth/register-user.dto'
+import { LoginUserDto } from '@/domain/dtos/auth/login-user.dto'
+import { AuthUseCases } from '@/domain/interfaces/auth.interface'
+import { CustomError } from '@/domain/errors/custom.error'
 
 export class AuthController {
-    static async login(req: Request, res: Response) {
-        res.json({
-            message: 'Login'
-        })
+    constructor(private readonly authUseCases: AuthUseCases) {}
+
+    private handleError = (error: any, res: Response) => {
+        if (error instanceof CustomError) {
+            return res.status(error.statusCode).json({ error: error.message })
+        }
+
+        return res.status(500).json({ error: 'Internal server error' })
     }
 
-    static async register(req: Request, res: Response) {
-        res.json({
-            message: 'Register'
-        })
+    loginUser = async (req: Request, res: Response) => {
+        const { email, password } = req.body
+        const [error, loginUserDto] = LoginUserDto.create(email, password)
+        if (error) return res.status(400).json({ error })
+
+        await this.authUseCases.loginUser
+            .execute(loginUserDto!)
+            .then((data) => res.json(data))
+            .catch((error) => this.handleError(error, res))
+    }
+
+    registerUser = async (req: Request, res: Response) => {
+        const [error, registerUserDto] = RegisterUserDto.create(req.body)
+        if (error) return res.status(400).json({ error })
+
+        await this.authUseCases.registerUser
+            .execute(registerUserDto!)
+            .then((data) => res.json(data))
+            .catch((error) => this.handleError(error, res))
     }
 }
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,12 +1,26 @@
 import { Router } from 'express'
 import { AuthController } from './controller'
+import { AuthRepositoryImpl } from '@/infrastructure/repositories/auth/auth.repository.impl'
+import { AuthDataSourceImpl } from '@/infrastructure/datasources/auth/auth.datasource.impl'
+import { LoginUserUseCase } from '@/domain/use-cases/auth/login.use-case'
+import { AuthUseCases } from '@/domain/interfaces/auth.interface'
+import { RegisterUserUseCase } from '@/domain/use-cases/auth/register.use-case'
 
 export class AuthRoutes {
     static get routes() {
         const router = Router()
+        const authDatasourceImpl = new AuthDataSourceImpl()
+        const authRepositoryImpl = new AuthRepositoryImpl(authDatasourceImpl)
 
-        router.get('/login', AuthController.login)
-        router.get('/register', AuthController.register)
+        const authUseCases: AuthUseCases = {
+            loginUser: new LoginUserUseCase(authRepositoryImpl),
+            registerUser: new RegisterUserUseCase(authRepositoryImpl)
+            //resetPassword: new ResetPasswordUseCase(userRepository),
+        }
+        const controller = new AuthController(authUseCases)
+
+        router.post('/login', controller.loginUser)
+        router.post('/register', controller.registerUser)
 
         return router
     }

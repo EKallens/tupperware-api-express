55fd29cb12db8972bcc7a77c32814b2b58e8223e
Edwin Kallens Padilla <e.kallensp@gmail.com>
2024-11-25 22:44:11 -0300

    feat: install new middlewares (cors and helmet)

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,7 @@
 import express, { Router } from 'express'
 import { logger } from '@/config/logger'
+import helmet from 'helmet'
+import cors from 'cors'
 
 type Options = {
     port?: number
@@ -22,6 +24,8 @@ export class Server {
             logger.info({ method: req.method, url: req.url, timestamp: new Date().toISOString() })
             next()
         })
+        this.app.use(cors())
+        this.app.use(helmet())
         this.app.use(express.json())
         this.app.use(express.urlencoded({ extended: true }))
 

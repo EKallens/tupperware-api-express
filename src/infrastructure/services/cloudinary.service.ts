2e72428f83b314169b5e0d7aab71d0b0d6cf68b8
EKallens <e.kallensp@gmail.com>
2025-01-07 19:38:56 -0300

    refactor: update cloudinary service to reduce the image size

--- src/infrastructure/services/cloudinary.service.ts
+++ src/infrastructure/services/cloudinary.service.ts
@@ -6,12 +6,20 @@ import { IFileService } from '@/domain/interfaces/file-service.interface'
 export class CloudinaryService implements IFileService {
     async uploadFile(filePath: string): Promise<string> {
         try {
-            const response = await cloudinary.uploader.upload(filePath, {}, (error, result) => {
-                if (error || !result) {
-                    logger.error('Error uploading the file', error)
-                    throw CustomError.internalServer('Error uploading the file, please check the logs')
+            const response = await cloudinary.uploader.upload(
+                filePath,
+                {
+                    width: 800,
+                    quality: 'auto:low',
+                    crop: 'limit'
+                },
+                (error, result) => {
+                    if (error || !result) {
+                        logger.error('Error uploading the file', error)
+                        throw CustomError.internalServer('Error uploading the file, please check the logs')
+                    }
                 }
-            })
+            )
 
             return response.secure_url
         } catch (error) {

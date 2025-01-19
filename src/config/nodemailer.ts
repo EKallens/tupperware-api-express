8aabc2414428d77ed425b40e2b588b246354fa2b
EKallens <e.kallensp@gmail.com>
2025-01-19 20:00:46 -0300

    fix: update nodemailer configuration

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -2,19 +2,6 @@ import nodemailer from 'nodemailer'
 import { google } from 'googleapis'
 import { envs } from './envs'
 
-// export const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: envs.MAIL_USERNAME,
-//         pass: envs.MAIL_PASSWORD,
-//         clientId: envs.OAUTH_CLIENT_ID,
-//         clientSecret: envs.OAUTH_CLIENT_SECRET,
-//         refreshToken: envs.OAUTH_REFRESH_TOKEN,
-//         accessToken:
-//             'ya29.a0ARW5m75YOYL-4MTp_4TcFzqJCeyetEzozcXOAiFTKnoJITqc2eeZnyl7pBnkfvD9P4eb4nj4edtO48BMiRfwzux9ufuG9di6-xUJkV3G4jNamAmSEEZKK9gOaBm0ehy-GFFXJhKbdsUwWsGMRrKYZepOLCSQ300_kDJ2H12AaCgYKAbISARISFQHGX2MiIqvW12P7KHduLKcHFClQPA0175'
-//     }
-// })
-
 const MAIL_USERNAME = envs.MAIL_USERNAME
 const MAIL_PASSWORD = envs.MAIL_PASSWORD
 const OAUTH_CLIENT_ID = envs.OAUTH_CLIENT_ID
